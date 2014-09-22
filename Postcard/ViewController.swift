39cbc64d06658f69f6fb945923aaad51ebc4cf29
Doug C <dchild@rollins.edu>
2014-09-21 23:11:05 -0400

    first version of project
    project 1

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet weak var hiddenLabel: UILabel!
+    
+    @IBOutlet weak var oneText: UITextField!
+
+    @IBOutlet weak var typeSomeText: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    @IBAction func sendMailButton(sender: AnyObject) {
+        hiddenLabel.text = oneText.text
+        hiddenLabel.sizeToFit()
+        // Need some code to make resizing work the first time
+        hiddenLabel.hidden = false
+        oneText.resignFirstResponder()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+    }
 
